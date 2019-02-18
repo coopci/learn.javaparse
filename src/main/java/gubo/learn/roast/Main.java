55169fa23d627fdc29769222d19a435f8b45dbbf
Gu Bo <bgu@zuora.com>
2019-02-18 10:42:20 +0800

    process folder recursively

--- src/main/java/gubo/learn/roast/Main.java
+++ src/main/java/gubo/learn/roast/Main.java
@@ -2,7 +2,12 @@ package gubo.learn.roast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.jboss.forge.roaster.Roaster;
 import org.jboss.forge.roaster.model.source.AnnotationSource;
@@ -15,20 +20,39 @@ public class Main {
 
 
 
-	public static void process(File f, JavaClassSourceProcessor p) throws FileNotFoundException {
+
+	public static void process(File f, JavaClassSourceProcessor processor, boolean doReplace) throws IOException {
 		if (f.isFile()) {
+			System.out.println("Processing " + f.toString() + "...");
 			JavaClassSource aClass = Roaster.parse(JavaClassSource.class, f);
 
-			String output = p.processClass(aClass);
+			String output = processor.processClass(aClass);
 			System.out.println(output);
-		}
 
+			if (doReplace) {
+
+				//
+			}
+
+		} else if (f.isDirectory()) {
+
+
+			List<Path> paths = Files.walk(Paths.get(f.toString()))
+					.filter(Files::isRegularFile)
+					.filter(f1 -> f1.toString().endsWith(".java"))
+					.collect(Collectors.toList());
+
+			for (Path p : paths) {
+				process(new File(p.toString()), processor, doReplace);
+			}
+		}
 	}
 
 
-	public static void main(String[] args) throws FileNotFoundException {
-		File f = new File("./src/main/java/gubo/learn/javaparser/tests/WebController.java");
-		process(f, new JavaClassSourceProcessor());
+	public static void main(String[] args) throws IOException {
+		// File f = new File("./src/main/java/gubo/learn/javaparser/tests/WebController.java");
+		File f = new File("./src/main/java/gubo/learn/javaparser/tests/");
+		process(f, new JavaClassSourceProcessor(), false);
 		
 
 
